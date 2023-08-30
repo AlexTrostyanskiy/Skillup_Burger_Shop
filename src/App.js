e5de3a60292ac11723881abb9840b5c770a8b60f
Alexander_Trostyanskiy <60698313+AlexTrostyanskiy@users.noreply.github.com>
2023-08-30 21:53:27 +0200

    shipping page

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 import Contact from "./components/contact/Contact";
 import Cart from "./components/cart/Cart";
+import Shipping from "./components/cart/Shipping";
 
-// Add module imports for Shipping
 
 import Login from "./components/login/Login";
 import Profile from "./components/profile/Profile";
@@ -38,18 +38,17 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<About />} />
-        <Route path="/cart" element={<Cart />} />      
-          //           Add the Route for SHIPPING
-        
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/shipping" element={<Shipping />} />
         <Route path="/login" element={<Login />} />
         <Route path="/me" element={<Profile />} />
          //           Add the Route for MY ORDERS
 
         <Route path="/order/:id" element={<OrderDetails />} />
-      
-       
 
-        
+
+
+
       </Routes>
 
       <Footer />
--- src/components/home/Menu.jsx
+++ src/components/home/Menu.jsx
@@ -1,3 +1,8 @@
+import MenuCard from "./MenuCard"
+import burger1 from "../../assets/burger1.png";
+import burger2 from "../../assets/burger2.png";
+import burger3 from "../../assets/burger3.png";
+
 const Menu = () => {
     const addToCartHandler = (itemNum) => { };
     return (
